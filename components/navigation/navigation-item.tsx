a8936bbd51e99c89cb01fe6106880d661f38adb5
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2023-10-09 16:22:24 +0700

    aodhj

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -14,9 +14,17 @@ interface NavigationItemProps {
 const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
   const params = useParams();
   const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  } 
+
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button className="group relative flex items-center">
+      <button 
+        className="group relative flex items-center"
+        onClick={onClick}  
+      >
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
