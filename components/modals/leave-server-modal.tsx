b72dccbe8cc71cbe22c6f17ea6fdc9da1bd0b03d
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2023-12-29 16:52:37 +0700

    aeofguaeigf

---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+import { useState } from "react";
+import axios from "axios";
+
+const LeaveServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const { server } = data;
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "leaveServer";
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+      onClose();
+      router.refresh();
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const handleClose = () => {
+    onClose();
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl font-bold text-center">
+            Leave Server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to leave <span className="font-semibold text-indigo-500">{server?.name}</span>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex justify-between w-full">
+            <Button variant="ghost" disabled={isLoading} onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" disabled={isLoading} onClick={onClick}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default LeaveServerModal;
